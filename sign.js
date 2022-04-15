6cbf2ac1faaadb818498d5c2499935aebf7bda5e
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-04-14 21:50:40 -0400

    update to use siwe

--- sign.js
+++ sign.js
@@ -1,17 +1,28 @@
 import { fromString as uint8arrayFromString } from "uint8arrays/from-string";
 import ethers from "ethers";
-
-const AUTH_SIGNATURE_BODY =
-  "I am creating an account to use Lit Protocol at {{timestamp}}";
+import siwe from "siwe";
 
 const privKey =
   "3dfb4f70b15b6fccc786347aaea445f439a7f10fd10c55dd50cafc3d5a0abac1";
 const privKeyBuffer = uint8arrayFromString(privKey, "base16");
-
-const now = new Date().toISOString();
-const messageToSign = AUTH_SIGNATURE_BODY.replace("{{timestamp}}", now);
-
 const wallet = new ethers.Wallet(privKeyBuffer);
+
+const domain = "localhost";
+const origin = "https://localhost/login";
+const statement =
+  "This is a test statement.  You can put anything you want here.";
+
+const siweMessage = new siwe.SiweMessage({
+  domain,
+  address: wallet.address,
+  statement,
+  uri: origin,
+  version: "1",
+  chainId: "1",
+});
+
+const messageToSign = siweMessage.prepareMessage();
+
 const signature = await wallet.signMessage(messageToSign);
 
 console.log("signature", signature);
