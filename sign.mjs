cc00e733a051f846206110e01f9c5f28665bae54
Ansonhkg <ansonox@gmail.com>
2024-02-08 17:50:34 +0000

    update

--- sign.mjs
+++ sign.mjs
@@ -1,6 +1,15 @@
 import { fromString as uint8arrayFromString } from "uint8arrays/from-string";
 import ethers from "ethers";
 import siwe from "siwe";
+import { LitNodeClient } from "@lit-protocol/lit-node-client";
+
+// Network available:
+// - https://developer.litprotocol.com/v3/network/networks/testnet/
+// - https://developer.litprotocol.com/v3/network/networks/mainnet
+const NETWORK = "manzano";
+
+const litNodeClient = new LitNodeClient({ network: NETWORK });
+await litNodeClient.connect();
 
 const privKey =
   "3dfb4f70b15b6fccc786347aaea445f439a7f10fd10c55dd50cafc3d5a0abac1";
@@ -12,8 +21,8 @@ const origin = "https://localhost/login";
 const statement =
   "This is a test statement.  You can put anything you want here.";
 const expirationTime = new Date(Date.now() + 60 * 60 * 1000).toISOString();
-let nonce; 
-//let nonce = litNodeClient.getLatestBlockhash(); get the latest nonce from the litNodeClient
+
+let nonce = litNodeClient.getLatestBlockhash();
 
 const siweMessage = new siwe.SiweMessage({
   domain,
@@ -30,39 +39,20 @@ const messageToSign = siweMessage.prepareMessage();
 
 const signature = await wallet.signMessage(messageToSign);
 
-console.log("signature", signature);
-
 const recoveredAddress = ethers.utils.verifyMessage(messageToSign, signature);
+
+if (recoveredAddress !== wallet.address) {
+  throw new Error("Recovered address does not match wallet address");
+}
+
+const latestBlockhash = litNodeClient.getLatestBlockhash();
+console.log("latestBlockhash", latestBlockhash);
 
 const authSig = {
   sig: signature,
   derivedVia: "web3.eth.personal.sign",
   signedMessage: messageToSign,
-  address: recoveredAddress,
+  address: await wallet.getAddress(),
 };
 
 console.log("authSig", authSig);
-
-//utility for generate auth signature
-export async function generateAuthSig(nonce: string) {
-  const expirationTime = new Date(Date.now() + 60 * 60 * 1000).toISOString();
-
-  const siweMessage = new SiweMessage({
-    domain,
-    address: await wallet.getAddress(),
-    statement: "Sign in to localhost",
-    uri: origin,
-    version: "1",
-    chainId: 1,
-    nonce,
-    expirationTime,
-  });
-  const messageToSign = siweMessage.prepareMessage();
-  const sig = await wallet.signMessage(messageToSign);
-  return {
-    sig,
-    derivedVia: "web3.eth.personal.sign",
-    signedMessage: messageToSign,
-    address: await wallet.getAddress(),
-  };
-}
