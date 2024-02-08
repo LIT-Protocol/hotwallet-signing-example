d704cc90cdd59abff1e366e6f7751d95d86eeada
Mike Liu <mike000708@me.com>
2024-02-08 12:03:04 -0500

    Update sign.js

--- sign.js
+++ sign.js
@@ -11,6 +11,9 @@ const domain = "localhost";
 const origin = "https://localhost/login";
 const statement =
   "This is a test statement.  You can put anything you want here.";
+const expirationTime = new Date(Date.now() + 60 * 60 * 1000).toISOString();
+let nonce; 
+//let nonce = litNodeClient.getLatestBlockhash(); get the latest nonce from the litNodeClient
 
 const siweMessage = new siwe.SiweMessage({
   domain,
@@ -19,6 +22,8 @@ const siweMessage = new siwe.SiweMessage({
   uri: origin,
   version: "1",
   chainId: "1",
+  nonce,
+  expirationTime,
 });
 
 const messageToSign = siweMessage.prepareMessage();
@@ -37,3 +42,27 @@ const authSig = {
 };
 
 console.log("authSig", authSig);
+
+//utility for generate auth signature
+export async function generateAuthSig(nonce: string) {
+  const expirationTime = new Date(Date.now() + 60 * 60 * 1000).toISOString();
+
+  const siweMessage = new SiweMessage({
+    domain,
+    address: await wallet.getAddress(),
+    statement: "Sign in to localhost",
+    uri: origin,
+    version: "1",
+    chainId: 1,
+    nonce,
+    expirationTime,
+  });
+  const messageToSign = siweMessage.prepareMessage();
+  const sig = await wallet.signMessage(messageToSign);
+  return {
+    sig,
+    derivedVia: "web3.eth.personal.sign",
+    signedMessage: messageToSign,
+    address: await wallet.getAddress(),
+  };
+}
